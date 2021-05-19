ebd0b1ad0fc183cf6ebee652b41797114e193ab6
Olaniran Sulaimon <suzzwayne@gmail.com>
2021-05-19 19:49:14 +0100

    add comments to auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -17,15 +17,20 @@ const { JWT_SECRET } = process.env
 
 const auth = (req, res, next) => {
    
+    //getting token from req header sent from client
     const token = req.header('x-auth-token')
     
 
     if (!token) return res.status(401).json({ "message": "Unauthorized, Please Log In" })
 
     try {
+        //get id and time from token sent from back end
+        //remember token was decoded using the user id and createdAt time
+        //decoded would equals = { id : user_id, createdAt : time_created}
 
         const decoded = jwt.verify(token, JWT_SECRET)
 
+        //saving the decoded data into a variable user in the request body
         req.user = decoded
 
         next()
